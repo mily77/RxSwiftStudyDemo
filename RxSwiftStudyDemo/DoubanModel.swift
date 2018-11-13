73228a203ce2ec978c9dd24d7681172a8aa87a37
mily77 <773258709@qq.com>
2018-11-13 11:24:24 +0800

    change

---
+++ RxSwiftStudyDemo/DoubanModel.swift
@@ -0,0 +1,51 @@
+//
+//  DoubanModel.swift
+//  RxSwiftStudyDemo
+//
+//  Created by emily on 2018/11/12.
+//  Copyright © 2018 emily. All rights reserved.
+//
+
+import UIKit
+import ObjectMapper
+
+//豆瓣接口模型
+class Douban: Mappable {
+    //频道列表
+    var channels: [Channel]?
+    
+    init(){
+    }
+    
+    required init?(map: Map) {
+    }
+    
+    // Mappable
+    func mapping(map: Map) {
+        channels <- map["channels"]
+    }
+}
+
+//频道模型
+class Channel: Mappable {
+    var name: String?
+    var nameEn:String?
+    var channelId: String?
+    var seqId: Int?
+    var abbrEn: String?
+    
+    init(){
+    }
+    
+    required init?(map: Map) {
+    }
+    
+    // Mappable
+    func mapping(map: Map) {
+        name <- map["name"]
+        nameEn <- map["name_en"]
+        channelId <- map["channel_id"]
+        seqId <- map["seq_id"]
+        abbrEn <- map["abbr_en"]
+    }
+}
--- RxSwiftStudyDemo/SessionViewController.swift
+++ RxSwiftStudyDemo/SessionViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import RxCocoa
 import RxSwift
-
+import Alamofire
 
 class SessionViewController: UIViewController {
 
@@ -61,25 +61,26 @@ class SessionViewController: UIViewController {
         }).disposed(by: disposeBag)
         
         //获取列表数据
-        let data = URLSession.shared.rx.json(request: request).map { (result) -> [[String: Any]] in
-            if let data = result as? [String: Any],let channels = data["channels"] as? [[String: Any]]{
-                return channels
-            
-            }else{
-                return []
-            }
+//        let data = URLSession.shared.rx.json(request: request).map { (result) -> [[String: Any]] in
+//            if let data = result as? [String: Any],let channels = data["channels"] as? [[String: Any]]{
+//                return channels
+//
+//            }else{
+//                return []
+//            }
+//        }
+        
+        let data = URLSession.shared.rx.json(request: request)
+            .mapObject(type: Douban.self)
+            .map {
+            $0.channels ?? []
         }
         
-        //将数据绑定到表格
-//        data.bind(to: tableView.rx.items) { (tableView, row, element) in
-//            let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")!
-//            cell.textLabel?.text = "\(row)：\(element["name"]!)"
-//            return cell
-//            }.disposed(by: disposeBag)
         
+        //将数据绑定到表格
         data.bind(to: tableView.rx.items) { (tableView, row, element) in
             let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")!
-            cell.textLabel?.text = "\(row)：\(element["name"]!)"
+            cell.textLabel?.text = "\(row)：\(element.name!)"
             return cell
         }.disposed(by: disposeBag)
     }
